5581a8e741860e276784ed87aae64293cfb53790
Guilherme Waess <guilherme.waess@gmail.com>
2018-03-07 16:04:52 +0100

    WIP testando formas de mostrar validacao na tela

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatchPassword } from '../../customValidators/matchPassword';
+import { matchPassword } from '../../customValidators/matchPassword';
+import { CustomValidators } from '../../customValidators/matchPassword';
 
 @Component({
   selector: 'app-sign-up',
@@ -21,11 +22,15 @@ export class SignUpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.signUpForm = new FormGroup({
-      'fullName': new FormControl(this.user.fullName, [Validators.minLength(10)]),
-      'email': new FormControl(this.user.email, [Validators.email]),
-      'password': new FormControl(this.user.password, [Validators.required]),
-    });
+    this.signUpForm = new FormGroup(
+      {
+        'fullName': new FormControl(this.user.fullName, [Validators.minLength(10)]),
+        'email': new FormControl(this.user.email, [Validators.email]),
+        'password': new FormControl(this.user.password, [Validators.required]),
+        'confirmPassword': new FormControl(this.user.confirmPassword, [CustomValidators.matchFields(this.signUpForm)]),
+      },
+      // { validators: CustomValidators.matchFields('password', 'confirmPassword') }
+    );
   }
 
 }
--- src/app/customValidators/matchPassword.ts
+++ src/app/customValidators/matchPassword.ts
@@ -1,12 +1,52 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, Validator, FormControl } from '@angular/forms';
 
-export class MatchPassword {
-  static MatchPassword(abstractControl: AbstractControl) {
-    let password = abstractControl.get('password').value;
-    let confirmPassword = abstractControl.get('confirmPassword').value;
+export function matchFields(fieldToCompare: string) {
+  return (control: AbstractControl) => {
+    debugger;
 
-    if (password !== confirmPassword) {
-      abstractControl.get('confirmPassword').setErrors({ MatchPassword: true });
-    }
+
+    return { matchFields: {} }
   }
 }
+
+export function matchPassword(abstractControl: AbstractControl) {
+  let password = abstractControl.get('password').value;
+  let confirmPassword = abstractControl.get('confirmPassword').value;
+
+  if (password !== confirmPassword) {
+    abstractControl.get('confirmPassword').setErrors({ MatchPassword: true });
+  }
+
+  return null;
+}
+
+export class CustomValidators {
+
+  static matchFields(fieldToCompareValue) {
+    return function (abstractControl: AbstractControl) {
+      let fieldValue = abstractControl.value;
+
+      if (fieldToCompareValue !== fieldValue) {
+        return { 'matchFields': 'Erro' }
+      }
+
+      return null;
+    }
+
+  }
+}
+
+
+// static matchFields(fieldToCompare) {
+//   return function (abstractControl: AbstractControl) {
+//     let fieldToCompareValue = abstractControl.get(fieldToCompare).value;
+//     let fieldValue = abstractControl.get(field).value;
+
+//     if (fieldToCompareValue !== fieldValue) {
+//       return { 'matchFields': { fieldToCompare, field } }
+//     }
+
+//     return null;
+//   }
+
+// }
